61486be9fc44fe60aea258131e635c030a2fbe32
azrozza shah <azrozzashah@gmail.com>
2022-09-16 17:34:07 +0100

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+airlines = Airline.create([
+  {name: "United Airlines",
+    image_url: "http://open-flights.s3.amazonaws.com/United-Airlines.png"},
+  {name: "Southwest Airlines",
+    image_url: "http://open-flights.s3.amazonaws.com/Southwest-Airlines.png"},
+  {name: "Delta",
+    image_url: "http://open-flights.s3.amazonaws.com/Delta.png"},
+  {name: "Alaska Airlines",
+    image_url: "http://open-flights.s3.amazonaws.com/Alaska-Airlines.png"},
+  {name: "JetBlue",
+    image_url: "http://open-flights.s3.amazonaws.com/JetBlue.png"},
+  {name: "American Airlines",
+    image_url: "http://open-flights.s3.amazonaws.com/American-Airlines.png"}
+])
+
+Review.create([
+
+  {title: 'Great airline',
+  description: 'I had a great time.',
+  score: 5,
+  airline: Airline.first},
+
+  {title: 'Terrible airline',
+    description: 'I had a terrible time.',
+    score: 1,
+    airline: Airline.first}
+    ])
