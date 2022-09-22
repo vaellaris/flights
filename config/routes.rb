91def1e20be7fc5bad52db42998fb5341057371c
azrozza shah <azrozzashah@gmail.com>
2022-09-22 13:17:08 +0100

    second debug

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :airlines, params :slug
+      resources :airlines, param: :slug
       resource :reviews, only: [:create, :destroy]
     end
   end
