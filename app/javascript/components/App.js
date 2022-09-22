3d4b687c6ca86ce4883991bd42415657b7bdd363
azrozza shah <azrozzashah@gmail.com>
2022-09-22 17:31:57 +0100

    made the routes and configured the react index js to apply all the routes that are for the App.js

---
+++ app/javascript/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {Route, Switch} from 'react-router-dom'
+
+const App = () => {
+  return (<h1>Hello World</h1>)
+}
+
+export default App;
--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -5,22 +5,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import App from '../components/App'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <Router>
+      # set up an indirect path for any route that will point initially to our App component
+      <Route path="/" components={App}/>
+    </Router>,
     document.body.appendChild(document.createElement('div')),
   )
 })
